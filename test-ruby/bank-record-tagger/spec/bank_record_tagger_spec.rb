92d27da2b452b2110d7acc4c60efc9b8655b0344
unknown <utlo6423@G-E6400-28.rd.francetelecom.fr>
2013-09-06 08:12:15 +0200

    bank-record-tagger: use rspec for tests

--- test-ruby/bank-record-tagger/spec/bank_record_tagger_spec.rb
+++ test-ruby/bank-record-tagger/spec/bank_record_tagger_spec.rb
@@ -4,7 +4,23 @@ describe BankRecordTagger do
   describe '#get_tags' do
     it 'returns no tag when tagger has not been configured' do
       tagger = BankRecordTagger.new
-      expect(tagger.get_tags("blab abala")).to eq(["rr"])
+      expect(tagger.get_tags("blab abala")).to eq([])
     end
+
+    it 'returns expected tags when tagger has been configured' do
+      tagger = BankRecordTagger.new
+      tagger.add_rule("PREL", "debit")
+      tagger.add_rule("EDF","electricity")
+
+      expect(tagger.get_tags("xxxxx EDF xxxx PRELEVEMENT xxx")).to eq(["debit","electricity"])
+    end
+
+    it 'returns no tag when no match' do
+      tagger = BankRecordTagger.new
+      tagger.add_rule("PREL", "debit")
+      tagger.add_rule("EDF","electricity")
+      expect(tagger.get_tags("blab abala")).to eq([])
+    end
+
   end
 end
